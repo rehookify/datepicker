fe84137f6c1b4adbed80c9293e0c3978ce4c7461
Pavlo Feshchenko <pavel.feschenko@gmail.com>
2023-11-03 11:57:02 +0200

    infra: move to pnpm workspace (#42)

--- packages/examples-e2e/playwright.config.ts
+++ packages/examples-e2e/playwright.config.ts
@@ -11,7 +11,7 @@ import { devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './e2e',
+  testDir: './test',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -99,8 +99,9 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start:app',
+    command: 'pnpm --filter examples dev',
     port: 5173,
+    reuseExistingServer: !process.env.CI,
   },
 };
 
---
+++ packages/examples/README.md
@@ -0,0 +1,6 @@
+# App for development and e2e testing
+
+## Development
+
+- install `pnpm` to your system `npm install -g pnpm`, like downloading chrome from the IE 🤗
+- run `pnpm dev` in the root of the repository
