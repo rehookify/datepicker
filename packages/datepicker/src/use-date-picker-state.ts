c85095138df0007e137a969fd9459bf816e21d8b
Feshchenko <pavel.feschenko@gmail.com>
2024-12-06 12:25:31 +0200

    chore: bump react to the v19

--- packages/datepicker/src/use-date-picker-state.ts
+++ packages/datepicker/src/use-date-picker-state.ts
@@ -1,21 +1,17 @@
-import { Reducer, useReducer } from 'react';
+import { useReducer } from 'react';
 
 import { stateReducer } from './state-reducer';
-import type {
-  DPReducerAction,
-  DPReducerState,
-  DPState,
-  DPUserConfig,
-} from './types';
+import type { DPState, DPUserConfig } from './types';
 import { createConfig } from './utils/config';
 import { createInitialState } from './utils/create-initial-state';
 
 export const useDatePickerState = (config: DPUserConfig): DPState => {
   const dpConfig = createConfig(config);
 
-  const [state, dispatch] = useReducer<
-    Reducer<DPReducerState, DPReducerAction>
-  >(stateReducer, createInitialState(dpConfig));
+  const [state, dispatch] = useReducer(
+    stateReducer,
+    createInitialState(dpConfig),
+  );
 
   return {
     dispatch,
